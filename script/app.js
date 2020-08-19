0657bb1c0b75c76727bbced6346b04fcedf734a0
Fidel Collin <fidelcollin@icloud.com>
2020-08-19 22:51:24 +0100

    updatefiles for navbar dropdown

--- script/app.js
+++ script/app.js
@@ -1,11 +1,17 @@
-const menu = document.querySelector(".menu-list");
-const toggleDigi = document.getElementById("toggle-btn");
-const toggleMenu = document.getElementById("toggle-menu");
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
-toggleMenu.addEventListener("click", () => {
-  if (toggleMenu) {
-    menu.style.display = `flex`;
-  } else {
-    menu.style.display = `none`;
-  }
+navToggle.addEventListener("click", () => {
+  // testing technique to check if classes are present to act on
+  //   console.log(links.classList);
+  //   console.log(links.classList.contains("random"));
+  //   console.log(links.classList.contains("links"));
+  // to toggle option 1---------------------
+  //   if (links.classList.contains("show-links")) {
+  //     links.classList.remove("show-links");
+  //   } else {
+  //     links.classList.add("show-links");
+  //   }---------------------------
+  //   to toggle option 2---------------
+  links.classList.toggle("show-links");
 });
