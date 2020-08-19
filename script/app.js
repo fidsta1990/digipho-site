ea187b5f2e2e26ea20343066c4dcef5f888704ab
Fidel Collin <fidelcollin@icloud.com>
2020-08-19 22:07:52 +0100

    updates to script, markup and css files

--- script/app.js
+++ script/app.js
@@ -1,3 +1,11 @@
-const menu = document.querySelector(".menu");
+const menu = document.querySelector(".menu-list");
 const toggleDigi = document.getElementById("toggle-btn");
 const toggleMenu = document.getElementById("toggle-menu");
+
+toggleMenu.addEventListener("click", () => {
+  if (toggleMenu) {
+    menu.style.display = `flex`;
+  } else {
+    menu.style.display = `none`;
+  }
+});
